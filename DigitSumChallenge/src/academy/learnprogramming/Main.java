08f1330343a1924952171630b2b378e38610b388
Gistinger Adrianne <Adrianne.Gistinger@elektrobit.com>
2021-11-16 13:58:35 -0500

    Completed challenges for for/while loops

--- DigitSumChallenge/src/academy/learnprogramming/Main.java
+++ DigitSumChallenge/src/academy/learnprogramming/Main.java
@@ -20,6 +20,32 @@ public class Main {
         System.out.println(hasSharedDigit(15, 55));
         System.out.println(hasSharedDigit(12, 13));
         System.out.println(hasSharedDigit(12, 43));
+
+        System.out.println(hasSameLastDigit(41, 22, 71));
+        System.out.println(hasSameLastDigit(23, 32, 42));
+        System.out.println(hasSameLastDigit(9, 99, 999));
+
+        System.out.println(getGreatestCommonDivisor(25, 15));
+        System.out.println(getGreatestCommonDivisor(12, 30));
+        System.out.println(getGreatestCommonDivisor(9, 18));
+        System.out.println(getGreatestCommonDivisor(81, 153));
+
+        printFactors(6);
+        printFactors(32);
+        printFactors(10);
+        printFactors(-1);
+
+        System.out.println(isPerfectNumber(6));
+        System.out.println(isPerfectNumber(28));
+        System.out.println(isPerfectNumber(5));
+        System.out.println(isPerfectNumber(-1));
+
+        System.out.println(reverse(-234));
+        System.out.println(getDigitCount(100));
+        System.out.println(getDigitCount(0));
+
+        numberToWords(100);
+        numberToWords(1450);
     }
 
     /*
@@ -119,34 +145,190 @@ public class Main {
         return -1;
     }
 
-        public static boolean hasSharedDigit(int num1, int num2) {
-            if(num1 >= 10 && num1 <= 99 && num2 >= 10 && num2 <= 99) {
+    public static boolean hasSharedDigit(int num1, int num2) {
+        if(num1 >= 10 && num1 <= 99 && num2 >= 10 && num2 <= 99) {
 
-                    // Loop over first number and turn into digits
-                    while(num1 > 0) {
-                        int lastDigit1 = num1 % 10;
-                        num1 /= 10;
+                // Loop over first number and turn into digits
+                while(num1 > 0) {
+                    int lastDigit1 = num1 % 10;
+                    num1 /= 10;
 
-                        // Loop over second number and turn into digits
-                        while(num2 > 0) {
-                            int lastDigit2 = num2 % 10;
-                            num2 /= 10;
+                    // Loop over second number and turn into digits
+                    while(num2 > 0) {
+                        int lastDigit2 = num2 % 10;
+                        num2 /= 10;
 
-                            // check if digits are equal
-                            if(lastDigit1 == lastDigit2) {
-                                return true;
-                            }
-                            // check if remaining numbers are equal
-                            if(num1 == num2) {
-                                return true;
-                            }
+                        // check if digits are equal
+                        if(lastDigit1 == lastDigit2) {
+                            return true;
+                        }
+                        // check if remaining numbers are equal
+                        if(num1 == num2) {
+                            return true;
                         }
                     }
+                }
+        }
+        return false;
+    }
+
+    public static boolean hasSameLastDigit(int numA, int numB, int numC) {
+       if(isValid(numA) && isValid(numB) && isValid(numC)) {
+            int lastA = numA % 10;
+            int lastB = numB % 10;
+            int lastC = numC % 10;
+
+           return lastA == lastB || lastC == lastB || lastA == lastC;
+       }
+       return false;
+    }
+
+    public static boolean isValid(int num) {
+        if(num >= 10 && num <= 1000) {
+            return true;
+        }
+        return false;
+    }
+
+    /*
+        If a = b, stop -- the GCD of aa and aa is, of course, aa. Otherwise, go to step 2.
+        If a > b, replace aa with a - ba−b, and go back to step 1.
+        If b > a, replace bb with b - ab−a, and go back to step 1.
+     */
+    public static int getGreatestCommonDivisor(int a, int b) {
+        if(a >= 10 && b >= 10) {
+
+            while(a != b) {
+                if(a > b) {
+                    a = a - b;
+                }
+                if(b > a)
+                    b = b - a;
             }
-            return false;
+            return a;
+        }
+        return -1;
+    }
+
+    public static void printFactors(int num) {
+        if(num >= 1) {
+            for(int i = 1; i <= num; i++) {
+                if(num % i == 0) {
+                    System.out.println(i);
+                }
+            }
+        } else {
+            System.out.println("Invalid Value");
+        }
+    }
+
+    public static boolean isPerfectNumber(int num) {
+        if(num >= 1) {
+            int addedNum = 0;
+            for(int i = 1; i < num; i++) {
+                if(num % i == 0) {
+                    addedNum += i;
+
+                    if(addedNum == num) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void numberToWords(int num) {
+        int reversed = reverse(num);
+        int digits = getDigitCount(num);
+        int reversedDigits = getDigitCount(reversed);
+
+        if(num >= 1) {
+            while(reversed > 0) {
+                int lastDigit = reversed % 10;
+                reversed /= 10;
+
+                switch(lastDigit) {
+                    case 1 :
+                        System.out.println("One");
+                        break;
+                    case 2 :
+                        System.out.println("Two");
+                        break;
+                    case 3 :
+                        System.out.println("Three");
+                        break;
+                    case 4 :
+                        System.out.println("Four");
+                        break;
+                    case 5 :
+                        System.out.println("Five");
+                        break;
+                    case 6 :
+                        System.out.println("Six");
+                        break;
+                    case 7 :
+                        System.out.println("Seven");
+                        break;
+                    case 8 :
+                        System.out.println("Eight");
+                        break;
+                    case 9 :
+                        System.out.println("Nine");
+                        break;
+                    case 0 :
+                        System.out.println("Zero");
+                        break;
+                }
+            }
+
+            if(reversedDigits < digits) {
+                for(int i = reversedDigits; i < digits; i++) {
+                    System.out.println("Zero");
+                }
+            }
+        } else if(num == 0){
+            System.out.println("Zero");
+        } else {
+            System.out.println("Invalid Value");
+        }
+    }
+
+    public static int reverse(int num) {
+        int reversed = 0;
+        boolean running = true;
+        boolean isNegative = num < 0;
+
+        if(isNegative) {
+            num *= -1;
         }
 
-        public int boolean hasSameLastDigit(int numA, int numB, int numC) {
+        while (running) {
+            if (num >= 10) {
+                int lastDigit = num % 10;
+                reversed += lastDigit;
+                reversed *= 10;
+                num /= 10;
+            } else {
+                reversed += num;
+                running = false;
+            }
+        }
+        return isNegative ? reversed *-1 : reversed;
+    }
 
+    public static int getDigitCount(int num) {
+        int digits = 0;
+        if(num == 0) {
+            return 1;
+        }
+        if(num >= 0) {
+            while (num > 0) {
+                num /= 10;
+                digits++;
+            }
+            return digits;
+        }
+        return -1;
     }
 }
